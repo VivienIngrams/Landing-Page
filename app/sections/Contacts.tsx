97b57084a5a3d72c888ea8e21039d925ffe5fe41
VivienIngrams <vivingrams@gmail.com>
2023-10-30 12:35:39 +0000

    New COntacts file

--- app/sections/Contacts.tsx
+++ app/sections/Contacts.tsx
@@ -1,8 +1,20 @@
+'use client'
+
 import React from 'react'
+
+import { motion } from "framer-motion";
+
+import styles from "../../styles";
+import { TypingText } from "../components/CustomTexts";
+import { staggerContainer, fadeIn } from "../../utils/motion";
+// import ContactForm from "../components/ContactForm";
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+        <section id="Contact"
+          className={`${styles.paddings} mt-20 relative h-full z-10 font-normal sm:text-[26px] text-[18px] text-center text-neutral-600`}
+        >
+            Contacts</section>
   )
 }
 
